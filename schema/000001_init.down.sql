d94e522d8492ed721309e3cabacdd1f4cd581265
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-11-22 12:18:07 +0300

    Update migrations (remove arrays)

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,9 +1,32 @@
 ALTER TABLE "notify_notifications" DROP CONSTRAINT notify_notifications_fk0;
 ALTER TABLE "notify_notifications" DROP CONSTRAINT notify_notifications_fk1;
+
 ALTER TABLE "notify_folders" DROP CONSTRAINT notify_folders_fk0;
+
 ALTER TABLE "refresh_tokens" DROP CONSTRAINT refresh_tokens_fk0;
 
+ALTER TABLE "bonds_collegues" DROP CONSTRAINT bonds_collegues_fk0;
+ALTER TABLE "bonds_collegues" DROP CONSTRAINT bonds_collegues_fk1;
+
+ALTER TABLE "bonds_subscriptions" DROP CONSTRAINT bonds_subscriptions_fk0;
+ALTER TABLE "bonds_subscriptions" DROP CONSTRAINT bonds_subscriptions_fk1;
+
+ALTER TABLE "notify_folders_participants" DROP CONSTRAINT notify_folders_participants_fk0;
+ALTER TABLE "notify_folders_participants" DROP CONSTRAINT notify_folders_participants_fk1;
+
+ALTER TABLE "notify_notifications_participants" DROP CONSTRAINT notify_notifications_participants_fk0;
+ALTER TABLE "notify_notifications_participants" DROP CONSTRAINT notify_notifications_participants_fk1S;
+
 DROP TABLE "users";
+
 DROP TABLE "refresh_tokens";
+
 DROP TABLE "notify_notifications";
 DROP TABLE "notify_folders";
+
+DROP TABLE "bonds_collegues";
+DROP TABLE "bonds_subscriptions";
+
+DROP TABLE "notify_folders_participants";
+DROP TABLE "notify_notifications_participants";
+
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -4,11 +4,27 @@ CREATE TABLE "users" (
 	"last_name" varchar NOT NULL,
 	"login" varchar NOT NULL UNIQUE,
 	"password_hash" varchar NOT NULL,
-	"colleagues" integer[] DEFAULT ARRAY[]::integer[],
-	"subscribers" integer[] DEFAULT ARRAY[]::integer[],
-	"subscriptions" integer[] DEFAULT ARRAY[]::integer[],
 	"color" integer NOT NULL CHECK(color >=0 AND color <= 16777215)
 );
+
+CREATE TABLE "bonds_collegues" (
+	"id" integer UNIQUE PRIMARY KEY,
+	"user1" integer NOT NULL,
+	"user2" integer NOT NULL,
+);
+
+ALTER TABLE "bonds_collegues" ADD CONSTRAINT "bonds_collegues_fk0" FOREIGN KEY ("user1") REFERENCES "users"("id");
+ALTER TABLE "bonds_collegues" ADD CONSTRAINT "bonds_collegues_fk1" FOREIGN KEY ("user2") REFERENCES "users"("id");
+
+
+CREATE TABLE "bonds_subscriptions" (
+	"id" integer UNIQUE PRIMARY KEY,
+	"from" integer NOT NULL,
+	"to" integer NOT NULL,
+);
+
+ALTER TABLE "bonds_subscriptions" ADD CONSTRAINT "bonds_subscriptions_fk0" FOREIGN KEY ("from") REFERENCES "users"("id");
+ALTER TABLE "bonds_subscriptions" ADD CONSTRAINT "bonds_subscriptions_fk1" FOREIGN KEY ("to") REFERENCES "users"("id");
 
 CREATE TABLE "refresh_tokens" (
 	"id" integer UNIQUE PRIMARY KEY,
@@ -30,6 +46,15 @@ CREATE TABLE "notify_folders" (
 
 ALTER TABLE "notify_folders" ADD CONSTRAINT "notify_folders_fk0" FOREIGN KEY ("owner") REFERENCES "users"("id");
 
+CREATE TABLE "notify_folders_participants" (
+	"id" integer UNIQUE PRIMARY KEY,
+	"folder" integer NOT NULL,
+	"user" integer NOT NULL
+);
+
+ALTER TABLE "notify_folders_participants" ADD CONSTRAINT "notify_folders_participants_fk0" FOREIGN KEY ("folder") REFERENCES "notify_folders"("id");
+ALTER TABLE "notify_folders_participants" ADD CONSTRAINT "notify_folders_participants_fk1" FOREIGN KEY ("user") REFERENCES "users"("id");
+
 CREATE TABLE "notify_notifications" (
 	"id" integer UNIQUE PRIMARY KEY,
 	"owner" integer NOT NULL,
@@ -37,9 +62,17 @@ CREATE TABLE "notify_notifications" (
 	"description" varchar(255),
 	"deadline" bigint,
 	"repeat" smallint,
-	"folder" integer,
-	"invited" integer[] DEFAULT ARRAY[]::integer[]
+	"folder" integer
 );
 
 ALTER TABLE "notify_notifications" ADD CONSTRAINT "notify_notifications_fk0" FOREIGN KEY ("owner") REFERENCES "users"("id");
 ALTER TABLE "notify_notifications" ADD CONSTRAINT "notify_notifications_fk1" FOREIGN KEY ("folder") REFERENCES "notify_folders"("id");
+
+CREATE TABLE "notify_notifications_participants" (
+	"id" integer UNIQUE PRIMARY KEY,
+	"notification" integer NOT NULL,
+	"user" integer NOT NULL
+);
+
+ALTER TABLE "notify_notifications_participants" ADD CONSTRAINT "notify_notifications_participants_fk0" FOREIGN KEY ("notification") REFERENCES "notify_notifications"("id");
+ALTER TABLE "notify_notifications_participants" ADD CONSTRAINT "notify_notifications_participants_fk1" FOREIGN KEY ("user") REFERENCES "users"("id");
